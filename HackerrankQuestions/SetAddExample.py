0046cc517d36c6b2939f01a9ecb5f1c9e57d82b2
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-03-02 17:00:53 +0300

    feat: add SetAddExample file

---
+++ HackerrankQuestions/SetAddExample.py
@@ -0,0 +1,37 @@
+"""
+Task
+Apply your knowledge of the .add() operation to help your friend Rupal.
+Rupal has a huge collection of country stamps. 
+She decided to count the total number of distinct country stamps in her collection. 
+She asked for your help. 
+You pick the stamps one by one from a stack of N country stamps.
+Find the total number of distinct country stamps.
+
+Input Format
+The first line contains an integer N, the total number of country stamps.
+The next N lines contains the name of the country where the stamp is from.
+
+Constraints
+0<N<1000
+
+Output Format
+Output the total number of distinct country stamps on a single line.
+
+Sample Input 0
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France 
+
+Sample Output 0
+5
+"""
+N = int(input())
+countries = set()
+for i in range(N):
+    countries.add(input())
+print(len(countries))
