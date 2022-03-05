8aea723df74b1ea0c992c4deb039588c3a31c33b
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-03-05 15:12:00 +0300

    feat: add StringValidatorsExample file

---
+++ HackerrankQuestions/StringValidatorsExample.py
@@ -0,0 +1,39 @@
+"""
+Task
+You are given a string S.
+Your task is to find out if the string S contains: 
+alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+
+Input Format
+A single line containing a string S.
+
+Constraints
+0<len(s)<1000
+
+Output Format
+
+In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
+In the second line, print True if S has any alphabetical characters. Otherwise, print False.
+In the third line, print True if S has any digits. Otherwise, print False.
+In the fourth line, print True if S has any lowercase characters. Otherwise, print False.
+In the fifth line, print True if S has any uppercase characters. Otherwise, print False.
+
+Sample Input 0
+qA2
+
+Sample Output 0
+True
+True
+True
+True
+True
+"""
+
+if __name__ == '__main__':
+    s = input()
+    if(len(s) > 0 and len(s) < 1000):
+        print(any(c.isalnum() for c in s))
+        print(any(c.isalpha() for c in s))
+        print(any(c.isdigit() for c in s))
+        print(any(c.islower() for c in s))
+        print(any(c.isupper() for c in s))
